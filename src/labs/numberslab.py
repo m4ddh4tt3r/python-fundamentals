056259bef348fbedca64713b262b8d4350c4cffc
m4ddh4tt3r <herotrip13@gmail.com>
2023-03-23 08:14:55 -0500

    Numbers Lab

---
+++ src/labs/numberslab.py
@@ -0,0 +1,29 @@
+""" Task 1 """
+
+
+var1 = 34_038
+var2 = 45_838.8734
+var3 = -3_695_365
+var4 = 0b_0110_0010_1011
+var5 = 0x1_74C0
+
+
+print(var1)
+print(var2)
+print(var3)
+print(var4)
+print(var5)
+
+
+""" Task 2 """
+
+
+def lit_val():
+    val1 = int(0x1D)
+    val2 = int(0b_0001_1101)
+    val3 = int(0o35)
+    print(f"Values = {val1}, {val2}, & {val3}")
+
+
+lit_val()
+
